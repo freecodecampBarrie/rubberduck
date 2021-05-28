b942405b791ba92f83967f055c602446c7602e36
Paul Gamble <pgamble@gmail.com>
2021-05-28 20:49:30 +0000

    more backend setup

---
+++ backend/routes/admin.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const adminController = require('../controllers/admin');
+const router = express.Router();
+
+router.get('/', adminController.getIndex);
+
+module.exports = router;
