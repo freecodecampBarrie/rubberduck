3b5f1d8d1e8bd1fed6e0f042532e05ab8824592e
Paul Gamble <pgamble@gmail.com>
2021-05-28 21:58:49 +0000

    backend and database setup

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
 router.get('/', adminController.getIndex);
+
+router.get('/add-duck', adminController.getAddDuck);
+
+router.post('/add-duck', adminController.postDuck);
 
 module.exports = router;
