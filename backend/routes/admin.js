620dd5e6caf8918fdc77dc25f410556019271c12
Paul Gamble <pgamble@gmail.com>
2021-06-05 12:17:36 -0400

    finished CRUD functions for backend as per MERN anime tutorial

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -7,6 +7,14 @@ router.get('/', adminController.getIndex);
 
 router.get('/add-duck', adminController.getAddDuck);
 
+router.get('/edit-duck/:duckId', adminController.getEditDuck);
+
 router.post('/add-duck', adminController.postDuck);
+
+router.post('/edit-duck', adminController.postEditDuck);
+
+router.get('/:duckId', adminController.getDuck);
+
+router.post('/delete', adminController.postDelete);
 
 module.exports = router;
